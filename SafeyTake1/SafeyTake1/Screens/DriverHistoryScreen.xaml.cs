f5fd49e2716abd6b70a0bbec9986d61a306f10c2
Guy Khmel <GuyKhmel@gmail.com>
2014-01-03 15:50:27 +0200

    Adding My History Screen

---
+++ SafeyTake1/SafeyTake1/Screens/DriverHistoryScreen.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace SafeyTake1.Screens
+{
+    public partial class DriverHistoryScreen : PhoneApplicationPage
+    {
+        public DriverHistoryScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
