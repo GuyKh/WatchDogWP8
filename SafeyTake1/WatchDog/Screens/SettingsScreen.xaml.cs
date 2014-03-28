abdb17c67b6ca75f114dee73cec1bcd785a67a0f
Guy Khmel <guykhmel@gmail.com>
2014-03-28 12:18:08 +0300

    Settings screen Back button goes back and not a new navigation

--- SafeyTake1/WatchDog/Screens/SettingsScreen.xaml.cs
+++ SafeyTake1/WatchDog/Screens/SettingsScreen.xaml.cs
@@ -24,7 +24,11 @@ namespace WatchDOG.Screens
 
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new Uri("/Screens/StartScreen.xaml", UriKind.Relative));
+            if (this.NavigationService.CanGoBack)
+                // Try going to the previous screen first
+                this.NavigationService.GoBack();
+            else 
+                NavigationService.Navigate(new Uri("/Screens/StartScreen.xaml", UriKind.Relative));
         }
     }
 }
