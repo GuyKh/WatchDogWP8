277574ea56856c2336983145521d59e1cb22dd96
danibuller <danibuller@mail.tau.ac.il>
2014-03-30 19:49:30 +0300

    Changes to LoginScreen

---
+++ SafeyTake1/WatchDog/Helpers/PhoneSettings.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using WatchDOG.DataStructures;
+
+namespace WatchDog.Helpers
+{
+    public class SettingsHelper
+    {
+        private const string SettingsDir = "Settings";
+        private const string SettingsFile = "settings.xml";
+
+        public static void SetSettings(Settings settings)
+        {
+            SaveSettingToFile<Settings>(SettingsDir, SettingsFile, settings);
+        }
+
+        public static Settings GetSettings()
+        {
+            return RetrieveSettingFromFile<Settings>(SettingsDir, SettingsFile);
+        }
+
+        private static T RetrieveSettingFromFile<T>(string dir, string file) where T : class
+        {
+            IsolatedStorageFile isolatedFileStore = IsolatedStorageFile.GetUserStoreForApplication();
+            if (isolatedFileStore.DirectoryExists(dir))
+            {
+                try
+                {
+                    using (var stream = new IsolatedStorageFileStream(System.IO.Path.Combine(dir, file), FileMode.Open, isolatedFileStore))
+                    {
+                        return (T)SerializationHelper.DeserializeData<T>(stream);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine("Could not retrieve file " + dir + "\\" + file + ". With Exception: " + ex.Message);
+                }
+            }
+            return null;
+        }
+
+        private static void SaveSettingToFile<T>(string dir, string file, T data)
+        {
+            IsolatedStorageFile isolatedFileStore = IsolatedStorageFile.GetUserStoreForApplication();
+            if (!isolatedFileStore.DirectoryExists(dir))
+                isolatedFileStore.CreateDirectory(dir);
+            try
+            {
+                string fn = System.IO.Path.Combine(dir, file);
+                if (isolatedFileStore.FileExists(fn)) isolatedFileStore.DeleteFile(fn); //mostly harmless, used because isolatedFileStore is stupid :D
+
+                using (var stream = new IsolatedStorageFileStream(fn, FileMode.CreateNew, FileAccess.ReadWrite, isolatedFileStore))
+                {
+                    SerializationHelper.SerializeData<T>(data, stream);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Could not save file " + dir + "\\" + file + ". With Exception: " + ex.Message);
+            }
+        }
+    }
+
+    public static class SerializationHelper
+    {
+        public static void SerializeData<T>(this T obj, Stream streamObject)
+        {
+            if (obj == null || streamObject == null)
+                return;
+
+            var ser = new DataContractJsonSerializer(typeof(T));
+            ser.WriteObject(streamObject, obj);
+        }
+
+        public static T DeserializeData<T>(Stream streamObject)
+        {
+            if (streamObject == null)
+                return default(T);
+
+            var ser = new DataContractJsonSerializer(typeof(T));
+            return (T)ser.ReadObject(streamObject);
+        }
+    }
+}
--- SafeyTake1/WatchDog/Screens/FirstTimeScreen.xaml.cs
+++ SafeyTake1/WatchDog/Screens/FirstTimeScreen.xaml.cs
@@ -7,27 +7,120 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using WatchDOG.DataStructures;
 
 namespace WatchDOG.Screens
 {
     public partial class FirstTime : PhoneApplicationPage
     {
-        private int _newSettings;
+        private Settings _newSettings;
+        private bool register = true;
 
         public FirstTime()
         {
             InitializeComponent();
         }
 
-        private void btnExit_Click(object sender, RoutedEventArgs e)
+        private void btnStatus_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchStatus();
+        }
+
+        /// <summary>
+        /// Switch UI statuses between Login and Register
+        /// </summary>
+        private void SwitchStatus()
+        {
+            if (register)
+            {
+                headline.Text = "Login";
+                txtVerify.Visibility = Visibility.Collapsed;
+                txtboxVerify.Visibility = Visibility.Collapsed;
+                btnState.Content = "Sign Up Mode";
+                register = false;
+            }
+            else
+            {
+                headline.Text = "Register";
+                txtVerify.Visibility = Visibility.Visible;
+                txtboxVerify.Visibility = Visibility.Visible;
+                btnState.Content = "Login Mode";
+                register = true;
+            }
+        }
+
+        protected override void OnBackKeyPress(System.ComponentModel.CancelEventArgs e)
         {
             Application.Current.Terminate();
         }
 
         private void btnContinue_Click(object sender, RoutedEventArgs e)
         {
+            if (!register)
+                if (!ValidateCredentials(txtboxUser.Text, txtboxPassword.Text))
+                {
+                    MessageBox.Show("Invalid username or password", "Error", MessageBoxButton.OK);
+                    return;
+                }
+                else
+                {
+                    Driver driver = LoadDriver(txtboxUser.Text);
+                }
+            else
+            {
+                if (txtboxPassword.Text != txtboxVerify.Text)
+                {
+                    MessageBox.Show("Both passwords do not match", "Error", MessageBoxButton.OK);
+                    return;
+                }
+                if (txtboxPassword.Text == "")
+                {
+                    MessageBox.Show("Password cannot be empty", "Error", MessageBoxButton.OK);
+                    return;
+                }
+
+                Settings.CurrentDriver = CreateNewDriver(txtboxName.Text, txtboxUser.Text, txtboxPassword.Text);
+                Settings.Loaded = true;
+            }
+
+            Settings.SaveSettingsToDisk();
+
             StartScreen.isFirstTime = false;
-            NavigationService.Navigate(new Uri("/Screens/SettingsScreen.xaml", UriKind.Relative));
+            if (NavigationService.CanGoBack)
+                NavigationService.GoBack();
+            else NavigationService.Navigate(new Uri("/Screens/SettingsScreen.xaml", UriKind.Relative));
+        }
+
+        private Driver LoadDriver(string username)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Create a new user from the username and password.
+        /// </summary>
+        /// <param name="name">Driver's Name</param>
+        /// <param name="username">Driver's Username</param>
+        /// <param name="password">Driver's Password</param>
+        private Driver CreateNewDriver(string name, string username, string password)
+        {
+            // ToDo: Insert to the DB.
+
+            Driver _driver = new Driver(name, username, password);
+            return _driver;
+        }
+
+        /// <summary>
+        /// Validate login credentials with the DB
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        private bool ValidateCredentials(string username, string password)
+        {
+            //ToDo: Check with the DB for existance of a username with this specific password.
+
+            return true;
         }
     }
 }
